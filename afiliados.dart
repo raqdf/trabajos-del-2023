556928fb534b296a415a7cde10888321afb9a467
Arlinton Ramon Mayorga Marin <143233654+raqdf@users.noreply.github.com>
2023-11-25 22:18:40 -0600

    Add files via upload

--- afiliados.dart
+++ afiliados.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Afiliados());
+void main() => runApp(MyApp());
 
-class Afiliados extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mecánicos Afiliados'),
+          title: Text('Lista de Afiliados'),
         ),
-        backgroundColor: const Color.fromARGB(255, 95, 95, 95),
         body: AffiliatesList(),
       ),
     );
@@ -27,41 +26,16 @@ class AffiliatesList extends StatelessWidget {
       itemCount: provider.getAffiliates().length,
       itemBuilder: (context, index) {
         final affiliate = provider.getAffiliates()[index];
-        return Card(
-          color: Color.fromARGB(
-              255, 241, 243, 241), // Color de fondo de la tarjeta
-          child: ListTile(
-            title: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'Nombre: ${affiliate.nombre} ${affiliate.apellidos}\n'
-                        'Nombre de usuario: ${affiliate.nombreUsuario}\n'
-                        'Ubicación: ${affiliate.localidad}\n'
-                        'Cédula de Identidad: ${affiliate.cedulaIdentidad}\n'
-                        'Fecha de Registro: ${affiliate.fechaRegistro}\n' // Agrega la fecha de registro
-                        'Hora de Registro: ${affiliate.horaRegistro}\n',
-                    style: TextStyle(
-                        color: Color.fromARGB(
-                            255, 8, 8, 8)), // Color del texto en negro
-                  ),
-                  TextSpan(
-                    text: 'Número de celular: ${affiliate.numeroCelular}\n',
-                    style: TextStyle(
-                      color: Colors
-                          .blue, // Cambia el color del número de celular a azul
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'Activo: ${affiliate.estado}',
-                    style: TextStyle(
-                      color: Colors
-                          .green, // Cambia el color del estado "Activo" a verde
-                    ),
-                  ),
-                ],
-              ),
-            ),
+        return ListTile(
+          title: Text('Nombre: ${affiliate.nombre} ${affiliate.apellidos}'),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Nombre de usuario: ${affiliate.nombreUsuario}'),
+              Text('Localidad: ${affiliate.localidad}'),
+              Text('Número de celular: ${affiliate.numeroCelular}'),
+              Text('Cédula de Identidad: ${affiliate.cedulaIdentidad}'),
+            ],
           ),
         );
       },
@@ -76,20 +50,14 @@ class Affiliate {
   final String localidad;
   final String numeroCelular;
   final String cedulaIdentidad;
-  final String estado;
-  final String fechaRegistro;
-  final String horaRegistro;
 
   Affiliate({
-    required this.nombre,
+    required this.nombre,//
     required this.apellidos,
     required this.nombreUsuario,
     required this.localidad,
     required this.numeroCelular,
     required this.cedulaIdentidad,
-    required this.estado,
-    required this.fechaRegistro,
-    required this.horaRegistro,
   });
 }
 
@@ -102,9 +70,6 @@ class AffiliatesProvider {
       localidad: 'Ciudad A',
       numeroCelular: '123-456-789',
       cedulaIdentidad: '1234567890',
-      estado: 'Activo',
-      fechaRegistro: '2023-10-19',
-      horaRegistro: '12:30 PM',
     ),
     Affiliate(
       nombre: 'María',
@@ -113,9 +78,6 @@ class AffiliatesProvider {
       localidad: 'Ciudad B',
       numeroCelular: '987-654-321',
       cedulaIdentidad: '9876543210',
-      estado: 'Activo',
-      fechaRegistro: '2023-10-20',
-      horaRegistro: '11:45 AM',
     ),
     // Agrega más afiliados con sus detalles aquí
   ];
--- paneldecitas.dart
+++ paneldecitas.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const PanelDeCitas());
+  runApp(MyApp());
 }
 
-class PanelDeCitas extends StatelessWidget {
-  const PanelDeCitas({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: CitasScreen(),
     );
@@ -17,8 +15,6 @@ class PanelDeCitas extends StatelessWidget {
 }
 
 class CitasScreen extends StatefulWidget {
-  const CitasScreen({super.key});
-
   @override
   _CitasScreenState createState() => _CitasScreenState();
 }
@@ -26,55 +22,19 @@ class CitasScreen extends StatefulWidget {
 class _CitasScreenState extends State<CitasScreen> {
   List<CitaMecanica> citas = [
     CitaMecanica(
-      mecanico: 'Orlando Mendoza',
-      cliente: 'Thomas Urtado',
-      direccion: 'De la pali 5 cuadras 1/2 al sur',
-      descripcion: 'Cambio de aceita',
-      id: 1,
-      fecha: '26 de octubre del 2023',
-      hora: '11:30 AM',
-      confirmada: true,
-    ),
+        id: 1, fecha: '29/10/2023', hora: '09:00 AM', confirmada: false),
     CitaMecanica(
-      mecanico: 'Warner Casablanca',
-      cliente: 'Osmar de Jesus Santillo',
-      direccion: 'Carretera la libertad kilometro 12',
-      descripcion: 'Calibracion del Volante',
-      id: 2,
-      fecha: '29 de Noviembre',
-      hora: '12:30 PM',
-      confirmada: true,
-    ),
+        id: 2, fecha: '26/10/2023', hora: '11:30 AM', confirmada: true),
+
     CitaMecanica(
-      mecanico: 'Jeysonh Tellez',
-      cliente: 'Osmar Josue Perez',
-      direccion: 'carretera managua',
-      descripcion: 'Llantas en mal estado',
-      id: 3,
-      fecha: '29 de Noviembre',
-      hora: '5:30 PM',
-      confirmada: true,
-    ),
+        id: 1, fecha: '21/10/2023', hora: '03:00 PM', confirmada: false),
     CitaMecanica(
-      mecanico: 'Carry Ortega Gonzales',
-      cliente: 'Miguel Cordoba',
-      direccion: 'Calle Xolotlan',
-      descripcion: 'Calibracion del Volante',
-      id: 4,
-      fecha: '29 de Noviembre',
-      hora: '4:30 PM',
-      confirmada: true,
-    ),
+        id: 2, fecha: '12/10/2023', hora: '11:50 AM', confirmada: true),
+
     CitaMecanica(
-      mecanico: 'Alex Mantiel',
-      cliente: 'Homero sinsomp',
-      direccion: 'Calle Coco',
-      descripcion: 'Calibracion del Volante',
-      id: 5,
-      fecha: '30 de Noviembre',
-      hora: '12:30 PM',
-      confirmada: true,
-    ),
+        id: 1, fecha: '19/10/2023', hora: '09:00 AM', confirmada: false),
+    CitaMecanica(
+        id: 2, fecha: '12/10/2023', hora: '04:30 PM', confirmada: true),
     // Agrega más citas según sea necesario
   ];
 
@@ -82,37 +42,39 @@ class _CitasScreenState extends State<CitasScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Panel de citas",
-          style: TextStyle(
-            color: Color.fromARGB(
-                255, 0, 0, 0), // Cambia el color del texto en el AppBar
-          ),
-        ),
-        backgroundColor: const Color.fromARGB(
-            255, 3, 118, 212), // Cambia el color de fondo del AppBar
+        title: Text("Citas de Revisión Mecánica"),
       ),
-      backgroundColor: const Color.fromARGB(
-          255, 137, 138, 138), // Cambia el color de fondo de la pantalla
       body: ListView.builder(
         itemCount: citas.length,
         itemBuilder: (context, index) {
           final cita = citas[index];
           return Card(
-            color: const Color.fromARGB(255, 241, 243,
-                241), // Cambia el color de fondo de la caja (Card) a verde
             child: ListTile(
-              title: Text('Mecánico: ${cita.mecanico}\n'
-                  'Cliente: ${cita.cliente}\n'
-                  'Dirección: ${cita.direccion}\n'
-                  'Descripción: ${cita.descripcion}\n'
-                  'Fecha: ${cita.fecha}\n'
-                  'Hora: ${cita.hora}\n'),
-              trailing: Icon(
-                cita.confirmada ? Icons.check : Icons.cancel,
-                color: cita.confirmada
-                    ? const Color.fromARGB(255, 10, 241, 2)
-                    : const Color.fromARGB(255, 236, 4, 4),
+              title: Text('Fecha: ${cita.fecha}'),
+              subtitle: Text('Hora: ${cita.hora}'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(cita.confirmada ? 'Confirmada' : 'Denegar'),
+                  IconButton(
+                    icon: Icon(Icons.cancel),
+                    onPressed: () {
+                      // Implementa la funcionalidad de cancelación de cita aquí
+                      setState(() {
+                        cita.confirmada = false;
+                      });
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.check),
+                    onPressed: () {
+                      // Implementa la funcionalidad de confirmación de cita aquí
+                      setState(() {
+                        cita.confirmada = true;
+                      });
+                    },
+                  ),
+                ],
               ),
             ),
           );
@@ -127,19 +89,11 @@ class CitaMecanica {
   String fecha;
   String hora;
   bool confirmada;
-  String mecanico;
-  String cliente;
-  String direccion;
-  String descripcion;
 
   CitaMecanica({
     required this.id,
     required this.fecha,
     required this.hora,
     required this.confirmada,
-    required this.mecanico,
-    required this.cliente,
-    required this.direccion,
-    required this.descripcion,
   });
 }
